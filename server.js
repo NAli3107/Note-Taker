4b0d89d78f0426f2dc02802ed518e318cafaef8f
Nadira Ali <nadiraali188a@gmail.com>
2022-03-07 19:22:01 +0000

    updated serve.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 3001; 
+const PORT = process.env.PORT || 3001; 
 
 const app = express(); 
 
@@ -13,6 +13,60 @@ app.use(express.json());
 app.use(express.urlencoded ({ extended: true }));
 app.use(express.static('public')); 
 
+/* Requesting data. */
+const { notes } = require('./db.json');
+
+const generateNewNotes = function (body, notesArr) {
+    const note = body;
+    notesArr.push(note);
+
+    fs.writeFileSync(
+        path.join(__dirname, './db.json'),
+        JSON.stringify({ notes : notesArr }, null, 2)
+    );
+    return note;
+};
+
+
+/* Route GET method */
+app.get('/api/notes', (req, res) => {
+    res.json(notes); 
+});
+
+/* Route POST method to store data server-side. */
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    
+        const note = generateNewNotes(req.body, notes); 
+
+        res.json(note);
+    
+}); 
+
+/* Delete method to remove notes. */
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    let note;
+
+    notes.map((element, index) => {
+      if (element.id == id){
+        note = element
+        notes.splice(index, 1)
+        return res.json(note);
+      } 
+    
+    })
+});
+
+/* Route to index.html */
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+}); 
+
+/* Route to notes.html */
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/notes.html'));
+}); 
 
 
 app.listen(PORT, () => {
