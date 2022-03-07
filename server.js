79587d46611fc82b861bc9bab89abbbda6443fc9
Nadira Ali <nadiraali188a@gmail.com>
2022-03-07 14:41:01 +0000

    rfectoring

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = process.env.PORT || 3001; 
+const PORT = 3001; 
 
 const app = express(); 
 
@@ -27,6 +27,17 @@ const generateNewNotes = function (body, notesArr) {
     return note;
 };
 
+/* validating data */
+function validateNote (note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false; 
+    }
+    if (!note.text || typeof note.text !== "string") {
+        return false;
+    }
+    return true;   
+};
+
 /* Route GET method */
 app.get('/api/notes', (req, res) => {
     res.json(notes); 
@@ -35,6 +46,17 @@ app.get('/api/notes', (req, res) => {
 /* Route POST method to store data server-side. */
 app.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
+
+    // if any data in req.body is incorrect, send error
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.'); 
+    
+    } else {
+        // add note to json file and animals array in this function 
+        const note = generateNewNotes(req.body, notes); 
+
+        res.json(note);
+    }
 }); 
 
 /* Delete method to remove notes. */
@@ -50,5 +72,20 @@ app.delete('/api/notes/:id', (req, res) => {
       } 
     
     })
+});
+
+/* Route to index.html */
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+}); 
+
+/* Route to notes.html */
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/notes.html'));
+}); 
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
 
